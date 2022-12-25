b9ae7c690eeb6912d4d84b1b6da6d063aefa85f3
Salmaihab53 <121397437+Salmaihab53@users.noreply.github.com>
2022-12-25 20:36:42 +0200

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,64 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the first num:");
+        int first_num = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the secound num:");
+        int secound_num = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("The prime numbers between {0} and {1} are:", first_num, secound_num);
+        for (int s = first_num; s <= secound_num; s++)
+        {
+            if (IsPrime(s))
+            {
+                Console.Write(s + " ");
+            }
+        }
+    }
+
+    static bool IsPrime(int number)
+    {
+        if (number < 2)
+        {
+            return false;
+        }
+
+        for (int s = 2; s <= Math.Sqrt(number); s++)
+        {
+            if (number % s == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,65 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System;
+
+namespace PerfectNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read y1 and y2 from the user
+            Console.WriteLine("Enter y1: ");
+            int y1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter y2: ");
+            int y2 = int.Parse(Console.ReadLine());
+
+            // Find and print the perfect numbers from y1 to y2
+            Console.WriteLine("Perfect numbers from " + y1 + " to " + y2 + ": ");
+            for (int z = y1; z <= y2; y++)
+            {
+                if (IsPerfectNumber(z))
+                {
+                    Console.WriteLine(z);
+                }
+            }
+        }
+
+        // Returns true if the given number is a perfect number, false otherwise
+        static bool IsPerfectNumber(int y)
+        {
+            int sum = 0;
+            for (int z = 1; z < y; z++)
+            {
+                if (y % z == 0)
+                {
+                    sum += z;
+                }
+            }
+            return sum == y;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
